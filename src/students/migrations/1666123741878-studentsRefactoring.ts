75a6a7907c63bcda6be7df83aed6d5188e9204d9
Henrique <hdsilveria@huia.haus>
2022-10-18 20:16:42 -0300

    create migration;

---
+++ src/students/migrations/1666123741878-studentsRefactoring.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class studentsRefactoring1666123741878 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
