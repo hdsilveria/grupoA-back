ed5ca263f7a21d68370416bbf53f454096a2ff42
Henrique <hdsilveria@huia.haus>
2022-10-19 19:49:50 -0300

    ajusted route to find by id;

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Post, Query, Res, Delete, Param } from '@nestjs/common';
+import { Response } from 'express';
 import { Student } from 'src/student.entity';
 import { ISearchStudent } from './students.interface';
 import { StudentsService } from './students.service';
@@ -9,24 +10,41 @@ export class StudentsController {
 
     @Get()
     findAll(): any {
-        return this.studentService.findAll()
+        return this.studentService.find()
+    }
+
+    @Get('id/:id')
+    getOne(@Param() id: number): any {
+        return this.studentService.find(id)
     }
 
     @Post()
-    create(@Body() user: Student){
-        return this.studentService.create(user)
+    create(@Body() user: Student, @Res() response: Response){
+        this.studentService.create(user).then(res => {
+            return response.status(200).json({
+                message: res
+            })
+        }).catch(err => {
+            return response.status(400).json({
+               err
+            })
+        })
     }
 
-    @Get('name')
+    @Get('student')
     search(@Query() student: ISearchStudent){
         return this.studentService.searchStudent(student.name)
     }
 
     @Post(':id')
-    update(@Body() student: Student, @Param() id: number, @Res() response){
+    update(@Body() student: Student, @Param() id: number, @Res() response: Response){
         this.studentService.update(id, student).then(res => {
             return response.status(200).json({
                 message: res
+            })
+        }).catch(err => {
+            return response.status(400).json({
+                err
             })
         })
     }
